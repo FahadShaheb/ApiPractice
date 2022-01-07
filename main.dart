135e0ea9a06496425aac4af71879f0f256b69864
Waliullah Fahad <57640854+FahadShaheb@users.noreply.github.com>
2022-01-07 16:07:11 +0600

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:todolist/home_screen.dart';
+import 'package:http/http.dart';
+import 'package:todolist/userdata.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData(
+      primaryColor: Colors.blue,
+    ),
+    home: const UserData(),
+  ));
+}
